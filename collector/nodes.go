f7a4243edd6d61693365b48b38d1b181dc7fd242
Christoph Oelmüller <christoph.oelmueller@justwatch.com>
2017-10-30 10:37:48 +0100

    fix role labels for ES 2.x

--- collector/nodes.go
+++ collector/nodes.go
@@ -34,7 +34,9 @@ var (
 		} else {
 			for role, setting := range node.Attributes {
 				if _, ok := roles[role]; ok {
-					roles[role] = setting == "false"
+					if setting == "false" {
+						roles[role] = false
+					}
 				}
 			}
 		}
